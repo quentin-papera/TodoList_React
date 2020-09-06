3ff951e13f5e395b4ecab0d601f01fed3d4b9b4e
Quentin Papera <qpapera@gmail.com>
2020-09-06 18:59:42 +1200

    Initial state

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
     {
       id: 2,
       title: 'Workout',
-      complete: false
+      complete: true
     },
     {
       id: 3,
@@ -21,10 +21,20 @@ class App extends Component {
     }
     ]
   }
+
+  markComplete = (id) => {
+		this.setState({todos:this.state.todos.map(todo => {
+      if(todo.id === id) {
+        todo.completed = !todo.completed
+      }
+      return todo
+    })})
+	  }
+
   render(){
     return(
       <div className="App">
-        <Todos todos={this.state.todos}/>
+        <Todos todos={this.state.todos} markComplete={this.markComplete}/>
       </div>
     )
   }
