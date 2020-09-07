a52d150aa0bb5d3053b21ac12ecdb4c95ed7dabb
Quentin Papera <qpapera@gmail.com>
2020-09-07 12:20:20 +1200

    Added About.js and create routes with react-router-dom

---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+	return (
+    <React.Fragment>
+      <h1>About</h1>
+      <p>This is my todo list built with React</p>
+    </React.Fragment>
+  
+	)
+}
+
+export default About
